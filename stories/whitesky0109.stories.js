4b093c90c10eb261a6c9e2d4dfbbb806c1b95170
GUNWOOCHO <gunwoo.cho@hanwha.com>
2024-03-04 15:29:25 +0900

    [assign/0] add hello world

---
+++ stories/whitesky0109.stories.js
@@ -0,0 +1,14 @@
+import HelloWorld from '../components/whitesky0109/helloWorld';
+
+export default {
+  title: 'whitesky0109/Hello world',
+  component: HelloWorld,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export const helloWorld = {};
